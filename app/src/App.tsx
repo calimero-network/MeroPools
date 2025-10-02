bcf8cd00afc551686601d67787a372a2e9f33a84
rtb-12 <ronit_c@me.iitr.ac.in>
2025-10-02 15:30:13 +0530

    feat: implement Providers component to manage application context and integrate VeChainKitProvider

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,16 +1,24 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import DashboardPage from "./pages/DashboardPage";
+import { Providers } from "./providers/Providers";
+import { Toaster } from "./components/ui/toaster";
+
 import "./App.css";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/dashboard" element={<DashboardPage />} />
-      </Routes>
-    </Router>
+    <Providers>
+      <>
+        <Router>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/dashboard" element={<DashboardPage />} />
+          </Routes>
+        </Router>
+        <Toaster />
+      </>
+    </Providers>
   );
 }
 
